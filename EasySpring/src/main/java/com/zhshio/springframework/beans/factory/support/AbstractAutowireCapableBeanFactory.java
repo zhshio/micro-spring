466874441cb024c9d8ddd62acd0f48b6b6050ee4
isodiary <130970883+isodiary@users.noreply.github.com>
2023-12-10 20:23:58 +0800

    docs: 为部分代码添加详细注释

--- EasySpring/src/main/java/com/zhshio/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ EasySpring/src/main/java/com/zhshio/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -10,15 +10,24 @@ import com.zhshio.springframework.beans.factory.config.BeanReference;
 import java.lang.reflect.Constructor;
 
 /**
- * @Auther: 张帅
- * @Date: 2023/11/9 - 11 - 09 - 19:56
- * @Description: com.zhshio.springframework.beans.factory.support
- * @version: 1.0
+ * 抽象的自动装配 Bean 工厂，继承自 AbstractBeanFactory。
  */
 public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory {
 
+    /**
+     * 实例化策略，默认使用 Cglib 子类化实例化策略。
+     */
     private InstantiationStrategy instantiationStrategy = new CglibSubclassingInstantiationStrategy();
 
+    /**
+     * 创建一个 bean 实例，并进行属性注入和初始化。
+     *
+     * @param beanName        bean 的名称
+     * @param beanDefinition  bean 的定义
+     * @param args            构造函数参数
+     * @return 创建好的 bean 实例
+     * @throws BeansException 如果实例化 bean 失败，抛出异常
+     */
     @Override
     protected Object createBean(String beanName, BeanDefinition beanDefinition, Object[] args) throws BeansException {
         Object bean = null;
@@ -26,13 +35,21 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
             bean = createBeanInstance(beanDefinition, beanName, args);
             applyPropertyValue(beanName, bean, beanDefinition);
         } catch (Exception e) {
-            throw new BeansException("Instantiation of bean failed", e);
+            throw new BeansException("实例化 bean 失败", e);
         }
 
         addSingleton(beanName, bean);
         return bean;
     }
 
+    /**
+     * 创建 bean 实例。
+     *
+     * @param beanDefinition  bean 的定义
+     * @param beanName        bean 的名称
+     * @param args            构造函数参数
+     * @return 创建好的 bean 实例
+     */
     protected Object createBeanInstance(BeanDefinition beanDefinition, String beanName, Object[] args) {
         Constructor constructorToUse = null;
         Class<?> beanClass = beanDefinition.getBeanClass();
@@ -46,12 +63,18 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
         return getInstantiationStrategy().instantiate(beanDefinition, beanName, constructorToUse, args);
     }
 
-
+    /**
+     * 对 bean 进行属性注入。
+     *
+     * @param beanName        bean 的名称
+     * @param bean            bean 实例
+     * @param beanDefinition  bean 的定义
+     * @throws BeansException 如果属性注入失败，抛出异常
+     */
     protected void applyPropertyValue(String beanName, Object bean, BeanDefinition beanDefinition) {
         try {
             PropertyValues propertyValues = beanDefinition.getPropertyValues();
             for (PropertyValue propertyValue : propertyValues.getPropertyValues()) {
-
                 String name = propertyValue.getName();
                 Object value = propertyValue.getValue();
 
@@ -62,17 +85,25 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
                 BeanUtil.setFieldValue(bean, name, value);
             }
         } catch (Exception e) {
-            throw new BeansException("Error setting property values：" + beanName);
+            throw new BeansException("设置属性值出错：" + beanName);
         }
     }
 
-
+    /**
+     * 获取实例化策略。
+     *
+     * @return 实例化策略
+     */
     public InstantiationStrategy getInstantiationStrategy() {
         return instantiationStrategy;
     }
 
+    /**
+     * 设置实例化策略。
+     *
+     * @param instantiationStrategy 实例化策略
+     */
     public void setInstantiationStrategy(InstantiationStrategy instantiationStrategy) {
         this.instantiationStrategy = instantiationStrategy;
     }
-
 }
